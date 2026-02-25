952057799043af4bf2bbc1b458d3d3a8b031ceb7
Georgina Ferguson Laing <georginalaing16@gmail.com>
2026-02-25 10:05:59 +0000

    Add Render build script

---
+++ ap_tracker/build.sh
@@ -0,0 +1,10 @@
+set -o errexit
+
+# Install python dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+python manage.py migrate
